21604133be9035ec8a9e1a6b9d28b41ca6b2f3f7
khaulabrian <khaulabrian2233@students.unnes.ac.id>
2024-03-08 16:32:02 +0700

    main

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesAdmins;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -19,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesAdmins;
+    use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
